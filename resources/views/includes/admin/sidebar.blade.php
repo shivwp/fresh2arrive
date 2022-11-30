e4d5feb8952c3b943d3cc875fe9fb82e804bd032
pramod kumar <pramodkumawat@eoxysitsolutionllp.com>
2022-11-30 18:15:24 +0530

    Added admin layout

---
+++ resources/views/includes/admin/sidebar.blade.php
@@ -0,0 +1,23 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+	<ul class="nav">
+		<li class="nav-item">
+			<a class="nav-link" href="/">
+				<i class="mdi mdi-grid-large menu-icon"></i>
+				<span class="menu-title">Dashboard</span>
+			</a>
+		</li>
+
+		<li class="nav-item">
+			<a class="nav-link" data-bs-toggle="collapse" href="#user" aria-expanded="false" aria-controls="user">
+				<i class="menu-icon mdi mdi-account-circle-outline"></i>
+				<span class="menu-title">Users Management</span>
+				<i class="menu-arrow"></i>
+			</a>
+			<div class="collapse" id="user">
+				<ul class="nav flex-column sub-menu">
+				<li class="nav-item"> <a class="nav-link" href="#"> Users </a></li>
+				</ul>
+			</div>
+		</li>
+	</ul>
+</nav>
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Star Admin2 </title>
+	<!-- plugins:css -->
+	<link rel="stylesheet" href="{{ asset('admin/assets/vendors/feather/feather.css') }}">
+	<link rel="stylesheet" href="{{ asset('admin/assets/vendors/mdi/css/materialdesignicons.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('admin/assets/vendors/ti-icons/css/themify-icons.css') }}">
+	<link rel="stylesheet" href="{{ asset('admin/assets/vendors/typicons/typicons.css') }}">
+	<link rel="stylesheet" href="{{ asset('admin/assets/vendors/simple-line-icons/css/simple-line-icons.css') }}">
+	<link rel="stylesheet" href="{{ asset('admin/assets/vendors/css/vendor.bundle.base.css') }}">
+	<!-- endinject -->
+	<!-- Plugin css for this page -->
+	<!-- <link rel="stylesheet" href="vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+	<link rel="stylesheet" href="js/select.dataTables.min.css"> -->
+	<!-- End plugin css for this page -->
+	<!-- inject:css -->
+	<link rel="stylesheet" href="{{ asset('admin/assets/css/vertical-layout-light/style.css') }}">
+	<!-- endinject -->
+	<link rel="shortcut icon" href="images/favicon.png" />
+</head>
+
+<body>
+	
+	<div class="container-scroller">
+		@include('includes.admin.header')
+		<div class="container-fluid page-body-wrapper">
+			@include('includes.admin.sidebar')
+			<div class="main-panel">
+				@yield('content')
+				@include('includes.admin.footer')
